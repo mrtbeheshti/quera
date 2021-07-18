3837ef0c42f1f9967a3cafaa28849848d00b6460
mrtbeheshti <mrtbeheshti@gmail.com>
2021-07-18 11:45:32 +0430

    Jadvalzarb updated but still doesn't work completely

--- JadvalZarb/JadvalZarb.py
+++ JadvalZarb/JadvalZarb.py
@@ -1,6 +1,5 @@
-a=int(input("enter your number:"))
-for i in range(1,a+1):
-    for j in range(1,a+1):
-        print(str(i*j)+" ",end='')
-    print("\b")
-input()
+a = int(input("enter your number:"))
+for i in range(1, a+1):
+    for j in range(1, a+1):
+        print(str(i*j), end=' ')
+    print()
